9fd2994fccec2178d12a3b20fdab5a8b65a8e0b3
Tocule <82139252+Tocule@users.noreply.github.com>
2022-03-27 21:46:51 +0530

    Update TowerofHanoi.hs

--- TowerofHanoi.hs
+++ TowerofHanoi.hs
@@ -3,9 +3,11 @@ data Tower = First|Middle|Last deriving (Show,Eq)
 f :: Tower
 m :: Tower
 l :: Tower
+
 f=First
 m=Middle
 l=Last
+
 type Step=(Integer,(Tower,Tower))
 
 solution :: Integer -> [Step]
@@ -14,7 +16,6 @@ solution 0 = []
 solution n = myHanoi n First Middle Last
 
 myHanoi :: Integer -> Tower -> Tower -> Tower -> [Step]
-
 
 myHanoi 1 f m l = [(1,(f,l))]
 myHanoi n f m l =
